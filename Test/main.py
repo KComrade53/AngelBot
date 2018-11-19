78bd25168d115992c5e344e93a2a116a635ac739
Kyle Conrad <kcomrade53@gmail.com>
2018-11-19 10:21:45 -0500

    Bot now opens quiz and answers A!

--- Test/main.py
+++ Test/main.py
@@ -8,6 +8,7 @@ from secrets import *
 frequency = 2500  # Set Frequency To 2500 Hertz
 duration = 1000  # Set Duration To 1000 ms == 1 second
 found = False
+foundButton = False
 
 driver = webdriver.Chrome("../drivers/chromedriver.exe")
 
@@ -22,7 +23,7 @@ time.sleep(2)  # delays it so next findElement doesn't run too early
 
 # Choose Angel course
 driver.find_element_by_partial_link_text('Denis').click()
-time.sleep(2)
+time.sleep(10)
 
 while not found:
     try:
@@ -40,6 +41,19 @@ while not found:
         print("Chrome closed")
         driver.quit()
         break   # exit while loop
+
+while 1:
+    try:
+        driver.find_element_by_id("multiple-choice-a").click()
+        time.sleep(20)
+        # foundButton = True
+    except NoSuchElementException:
+        print("Failed to find buttons, repeat\n")
+        time.sleep(15)
+    except ElementNotVisibleException:
+        print("Button not visible yet\n")
+        time.sleep(15)
+
 
 # print(pew)
 # driver.find_element_by_id('course-history-icon').click()  # for testing on course page
